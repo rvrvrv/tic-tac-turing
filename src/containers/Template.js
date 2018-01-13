0235e0d10b835bc884397da4ceb06ef0785fce8a
rvrvrv <ibanez19@sbcglobal.net>
2018-01-13 10:12:36 -0600

    Implement material-ui and react-tap-event

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 export default class Template extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <h1>Tic-Tac-Turing</h1>
-        </header>
-        <main>
-          {this.props.children}
-        </main>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <header>
+            <h1>Tic-Tac-Turing</h1>
+            <RaisedButton
+              label={'Test'}
+              primary={true}
+              onTouchTap={()=>{console.log('test')}}/>
+          </header>
+          <main>
+            {this.props.children}
+          </main>
+        </div>
+      </MuiThemeProvider>
     )
   }
 };
