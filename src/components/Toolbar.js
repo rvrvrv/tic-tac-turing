d61b06c75d64fecce2ec6713c67a2bc920e421b8
rvrvrv <ibanez19@sbcglobal.net>
2018-01-14 13:36:19 -0600

    Remove underlines from Link components

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { AppBar, Popover, Menu, MenuItem } from 'material-ui';
 
 export default class Toolbar extends Component {
@@ -40,8 +41,16 @@ export default class Toolbar extends Component {
           zDepth={3}
         >
           <Menu>
-            <MenuItem primaryText="Play Game" />
-            <MenuItem primaryText="Profile" />
+            <Link to={'/'} style={{ textDecoration: 'none' }}>
+              <MenuItem
+                onTouchTap={this.handleRequestClose}
+                primaryText="Play Game" />
+            </Link>
+            <Link to={'/profile'} style={{ textDecoration: 'none' }}>
+              <MenuItem
+                onTouchTap={this.handleRequestClose}
+                primaryText="Profile" />
+            </Link>
           </Menu>
         </Popover>
       </div>
