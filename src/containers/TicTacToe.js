5b0a3a619025d3709b7beb5f5a004604c9dcb190
rvrvrv <ibanez19@sbcglobal.net>
2018-01-15 08:55:02 -0600

    Replace Home container with TicTacToe

---
+++ src/containers/TicTacToe.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class TicTacToe extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentWillMount() {
+
+  }
+
+  move = () => {
+
+  }
+
+  aiMove = () => {
+
+  }
+
+  turingTest = () => {
+
+  }
+
+  recordGame = () => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <Board /> */}
+        {/* <Squares /> */}
+      </div>
+    )
+  }
+};
