045955ee74916a78867d97789109e1990d3f8199
rvrvrv <ibanez19@sbcglobal.net>
2018-01-16 09:29:00 -0600

    Build out authentication

---
+++ src/utils/auth.js
@@ -0,0 +1,71 @@
+import Auth0Lock from 'auth0-lock';
+const authDomain = 'rvrvrv.auth0.com';
+const clientId = 'HcMolKSmeIq3cmA577qNRsjiQre7Is7t';
+
+export default class Auth {
+  constructor() {
+    this.lock = new Auth0Lock(clientId, authDomain, {
+      auth: {
+        params: {
+          scope: 'openid email'
+        },
+      }
+    });
+    this.showLock = this.showLock.bind(this);
+    this.lock.on('authenticated', this.authProcess.bind(this));
+  }
+
+  authProcess = (authResult) => {
+    console.log(authResult);
+  }
+
+  showLock() {
+    this.lock.show();
+  }
+
+  // Store authFields in localStorage
+  setToken = (authFields) => {
+    let { idToken, exp } = authFields;
+    localStorage.setItem('rvrvrv-ttt-idToken', idToken);
+    localStorage.setItem('rvrvrv-ttt-exp', exp * 1000);
+  }
+
+  // Ensure auth is current (hasn't expired)
+  isCurrent = () => {
+    let expStr = localStorage.getItem('rvrvrv-ttt-exp');
+    if (!expString) {
+      localStorage.removeItem('rvrvrv-ttt-idToken');
+      return false;
+    }
+    // Compare current (now) and exp time
+    let now = new Date();
+    let exp = new Date(parseInt(expString, 10));
+    // If expired, log out
+    if (exp < now) {
+      this.logout();
+      return false;
+    } else return true; // If auth is current, return true
+  }
+
+  // Retrieve authFields from localStorage
+  getToken() {
+    let idToken = localStorage.getItem('rvrvrv-ttt-idToken');
+    if (this.isCurrent && idToken) return idToken;
+    // If expired, clear localStorage
+    else {
+      localStorage.removeItem('rvrvrv-ttt-idToken');
+      localStorage.removeItem('rvrvrv-ttt-exp');
+      return false;
+    }
+  }
+
+  // Log out user
+  logout = () => {
+    // Clear localStorage
+    localStorage.removeItem('rvrvrv-ttt-idToken');
+    localStorage.removeItem('rvrvrv-ttt-exp');
+    // Reload the page
+    location.reload();
+  }
+
+}
