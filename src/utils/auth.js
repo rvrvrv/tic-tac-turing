73888810913f9b87e37f9f4121d6423eefe2b0c4
rvrvrv <ibanez19@sbcglobal.net>
2018-01-18 12:33:51 -0600

    Change capitalization due to Heroku bug

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 import Auth0Lock from 'auth0-lock';
 import Relay from 'react-relay';
 import CreateUser from '../mutations/CreateUser';
-import SigninUser from '../mutations/SignInUser';
+import SigninUser from '../mutations/SigninUser';
 
 const authDomain = 'rvrvrv.auth0.com';
 const clientId = 'HcMolKSmeIq3cmA577qNRsjiQre7Is7t';
