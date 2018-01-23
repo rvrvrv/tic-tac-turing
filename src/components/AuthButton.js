372c0867fdfdd919dd747e956e3b3cb66a92b119
rvrvrv <ibanez19@sbcglobal.net>
2018-01-23 09:12:43 -0600

    Style AuthButton

--- src/components/AuthButton.js
+++ src/components/AuthButton.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import FlatButton from 'material-ui/FlatButton';
+import LockIcon from 'material-ui/svg-icons/action/lock-open';
+import { grey800, grey900 } from 'material-ui/styles/colors';
 
 export default class AuthButton extends Component {
   static muiName = 'FlatButton';
@@ -9,7 +11,9 @@ export default class AuthButton extends Component {
       return (
         <FlatButton
           style={this.props.style}
-          label='Log Out'
+          backgroundColor={grey800}
+          hoverColor={grey900}
+          label="Log Out"
           onTouchTap={this.props.auth.logout}
         />
       );
@@ -17,7 +21,8 @@ export default class AuthButton extends Component {
       return (
         <FlatButton
           style={this.props.style}
-          label='Log In'
+          label="Log In"
+          icon={<LockIcon />}
           onTouchTap={this.props.auth.showLock}
         />
       );
